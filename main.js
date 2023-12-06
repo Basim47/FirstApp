3b21766cf09f3d89ae2d0141866da6f4623274dd
Basim47 <mbasim465@gmail.com>
2023-12-06 18:22:49 +0500

    add redochanges

--- main.js
+++ main.js
@@ -42,7 +42,10 @@ const Main = () => {
     // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
     OneSignal.Notifications.requestPermission(true);
 
-
+    // Method for listening for notification clicks
+    OneSignal.Notifications.addEventListener('click', (event) => {
+      console.log('OneSignal: notification clicked:', event);
+    });
   }, [])
 
 
