c8a197524a1b8210c88096c3717a2e7c08d875e6
FahadIqball <fahadiqbalaps@gmail.com>
2023-12-16 15:19:15 +0500

    add Sign in screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
 //Nav
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 //Firebase
 import auth from '@react-native-firebase/auth';
 //Redux
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './src/store';
 //Screens
 import Onboard from './src/navigation/onboard';
@@ -19,12 +19,12 @@ const App = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState(null);
 
-  const onAuthStateChanged = (user) => {
+  const onAuthStateChanged = user => {
     if (user) {
       setUser(user);
     } else setUser(null);
     if (initializing) setInitializing(false);
-  }
+  };
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
@@ -34,17 +34,19 @@ const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerShown: false
-        }}>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
           {user ? (
             <>
-              <Stack.Screen name='AppStack' component={AppStack} />
+              <Stack.Screen name="AppStack" component={AppStack} />
             </>
           ) : (
             <>
-              <Stack.Screen name="Onboard" component={Onboard} />
               <Stack.Screen name="AuthStack" component={AuthStack} />
+              <Stack.Screen name="Onboard" component={Onboard} />
+              {/* <Stack.Screen name="AuthStack" component={AuthStack} /> */}
             </>
           )}
         </Stack.Navigator>
--- src/screens/signin.js
+++ src/screens/signin.js
@@ -1,14 +1,102 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import Colors from '../assets/colors/colors';
+import Fonts from '../assets/fonts/fonts';
 
 const Signin = () => {
-    return (
-        <View>
-            <Text>Signin</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.mainwrapper}>
+      <View style={styles.headwrapper}>
+        <Text style={styles.headtxt}>Daily Dose of Wisdom</Text>
+      </View>
+      <View style={styles.mainbody}>
+        <Text style={styles.regtxt}>Register</Text>
+        <Text style={styles.placeholdertxt}>Name</Text>
+        <TextInput
+          style={styles.placeholderinput}
+          placeholder="Enter your full name"
+          placeholderTextColor={Colors.white}
+        />
+        <Text style={styles.placeholdertxt}>Email</Text>
+        <TextInput
+          style={styles.placeholderinput}
+          placeholder="Enter your email"
+          placeholderTextColor={Colors.white}
+        />
+        <Text style={styles.placeholdertxt}>Password</Text>
+        <TextInput
+          style={styles.placeholderinput}
+          placeholder="Enter your password"
+          placeholderTextColor={Colors.white}
+        />
+        <Text style={styles.passcond}>
+          The password must have at least one lowercase, one uppercase, one
+          special and one numeric character.
+        </Text>
+        <Text style={styles.placeholdertxt}>Confirm Password</Text>
+        <TextInput
+          style={styles.placeholderinput}
+          placeholder="Enter your password"
+          placeholderTextColor={Colors.white}
+        />
+        <TouchableOpacity>
+          <Text>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-export default Signin
+export default Signin;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainwrapper: {
+    flex: 1,
+    backgroundColor: Colors.blue,
+  },
+  headwrapper: {
+    width: '100%',
+    marginTop: 130,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headtxt: {
+    fontSize: 13,
+    color: Colors.skin,
+    fontFamily: Fonts.regular,
+  },
+  mainbody: {
+    paddingHorizontal: 20,
+  },
+  regtxt: {
+    marginTop: 35,
+    fontSize: 18,
+    color: Colors.white,
+    fontFamily: Fonts.bold,
+  },
+  placeholdertxt: {
+    color: Colors.white,
+    fontSize: 14,
+    fontFamily: Fonts.regular,
+    marginTop: 15,
+  },
+  placeholderinput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: Colors.white,
+    color: Colors.white,
+    paddingLeft: 20,
+    marginTop: 7,
+  },
+  passcond: {
+    color: Colors.skin,
+    marginTop: 3,
+    fontSize: 13,
+  },
+});
