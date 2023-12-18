90733b825be13ef51defe91f8729eaa20edf5c77
Vampie0 <alibhatti6382@gmail.com>
2023-12-18 18:02:18 +0500

    Added Suggestion Screen

--- src/screens/suggestion.js
+++ src/screens/suggestion.js
@@ -1,17 +1,53 @@
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import React from 'react';
+// Redux
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../store/actions/themeSlice';
+// Components
 import Colors from '../assets/colors/colors';
+import Fonts from '../assets/fonts/fonts';
+// Icons
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 
 const Suggestion = () => {
+  const themeMode = useSelector(state => state.theme.mode);
   return (
-    <View>
+    <ScrollView style={[styles.mainwrapper, { backgroundColor: themeMode.background }]}>
       <StatusBar translucent
         backgroundColor={Colors.blue} />
-      <Text>Suggestion</Text>
-    </View>
+      <View style={styles.headwrapper}>
+        <TouchableOpacity
+          style={styles.mainbody}
+          onPress={() => navigation.goBack()}>
+          <AntDesign name={'arrowleft'} size={20} color={themeMode.text} />
+        </TouchableOpacity>
+        <Text style={[styles.headtxt, { color: themeMode.text }]}>Suggestion Box</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 export default Suggestion;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainwrapper: {
+    flex: 1,
+    backgroundColor: Colors.blue,
+  },
+  headwrapper: {
+    flexDirection: 'row',
+    marginTop: 60,
+    width: '100%',
+    alignItems: 'center',
+  },
+  mainbody: {
+    marginLeft: 20,
+  },
+  headtxt: {
+    fontFamily: Fonts.bold,
+    fontSize: 15,
+    color: Colors.white,
+    marginHorizontal: 110,
+  },
+});
