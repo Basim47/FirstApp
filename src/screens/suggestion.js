85c80b54d0508d994b4d4ef2efc914ddc0006401
Vampie0 <alibhatti6382@gmail.com>
2023-12-18 20:33:14 +0500

    Added Suggestion box

--- src/screens/suggestion.js
+++ src/screens/suggestion.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ScrollView, TouchableOpacity, TextInput, } from 'react-native';
 import React from 'react';
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
@@ -6,11 +6,12 @@ import { toggleTheme } from '../store/actions/themeSlice';
 // Components
 import Colors from '../assets/colors/colors';
 import Fonts from '../assets/fonts/fonts';
+import Btn from '../assets/components/btn';
 // Icons
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
-const Suggestion = () => {
+const Suggestion = ({ navigation }) => {
   const themeMode = useSelector(state => state.theme.mode);
   return (
     <ScrollView style={[styles.mainwrapper, { backgroundColor: themeMode.background }]}>
@@ -23,6 +24,16 @@ const Suggestion = () => {
           <AntDesign name={'arrowleft'} size={20} color={themeMode.text} />
         </TouchableOpacity>
         <Text style={[styles.headtxt, { color: themeMode.text }]}>Suggestion Box</Text>
+      </View>
+      <TextInput style={styles.placestyle}
+        placeholder="Write your Suggestions here..."
+        placeholderTextColor={Colors.grey} />
+      <View style={styles.btnview}>
+        <TouchableOpacity >
+          <Btn>
+            <Text style={styles.btntxt}>Send</Text>
+          </Btn>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -48,6 +59,27 @@ const styles = StyleSheet.create({
     fontFamily: Fonts.bold,
     fontSize: 15,
     color: Colors.white,
-    marginHorizontal: 110,
+    marginHorizontal: 80,
+  },
+  placestyle: {
+    height: 375,
+    alignSelf: 'center',
+    width: '90%',
+    marginTop: 43,
+    borderWidth: 1,
+    borderRadius: 7,
+    paddingLeft: 20,
+    borderColor: Colors.grey,
+    fontFamily: Fonts.medium,
+    color: Colors.white,
+    textAlignVertical: 'top'
+  },
+  btnview: {
+    marginTop: 35,
+  },
+  btntxt: {
+    fontSize: 14,
+    fontFamily: Fonts.medium,
+    color: Colors.white,
   },
 });
