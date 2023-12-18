11a2d513988f87bed572143f09ddf52f48e44ee6
Basim47 <mbasim465@gmail.com>
2023-12-18 16:35:17 +0500

    add minor change

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,28 +1,43 @@
-import { StyleSheet, Text, TouchableOpacity, View, StatusBar, Alert } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  StatusBar,
+  Alert,
+} from 'react-native';
 import React, { useEffect } from 'react';
-import { logout } from '../services/firebaseServices';
 import Colors from '../assets/colors/colors';
 import { setUserData } from '../store/actions/userAction';
 import { useDispatch } from 'react-redux';
-import auth from '@react-native-firebase/auth'
-import firestore from '@react-native-firebase/firestore'
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const Main = ({ navigation }) => {
   const dispatch = useDispatch();
 
-
-
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userId = auth().currentUser.uid;
+        const userId = auth().currentUser;
         if (userId) {
-          const userDocument = await firestore().collection('users').doc(userId).get();
+          const userDocument = await firestore()
+            .collection('users')
+            .doc(userId.uid)
+            .get();
           if (userDocument.exists) {
-            dispatch(setUserData({ ...userDocument.data(), documentId: userDocument.id }));
+            dispatch(
+              setUserData({
+                ...userDocument.data(),
+                documentId: userDocument.id,
+              }),
+            );
+          } else {
+            Alert.alert('no data');
           }
-          else { }
-        } else { }
+        } else {
+          Alert.alert('no user');
+        }
       } catch (error) {
         console.log(error);
       }
@@ -32,8 +47,7 @@ const Main = ({ navigation }) => {
 
   return (
     <View>
-      <StatusBar translucent
-        backgroundColor={Colors.blue} />
+      <StatusBar translucent backgroundColor={Colors.blue} />
       <View style={styles.headwrap}></View>
       <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
         <View style={{ width: 70, height: 70, backgroundColor: '#000' }}>
@@ -48,8 +62,8 @@ export default Main;
 
 const styles = StyleSheet.create({
   headwrap: {
-    width: "100%",
+    width: '100%',
     height: 40,
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
