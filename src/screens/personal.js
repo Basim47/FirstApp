3b38da4c01a2b49ab6109fdec330e6c1499d7899
Vampie0 <alibhatti6382@gmail.com>
2023-12-20 18:50:24 +0500

    added personal screen 2

--- src/screens/personal.js
+++ src/screens/personal.js
@@ -15,6 +15,10 @@ import Fonts from '../assets/fonts/fonts';
 // Icons
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+// toptab
+
+// Screens
+
 const Personal = ({ navigation }) => {
     const themeMode = useSelector(state => state.theme.mode);
     return (
